6225615b2e61bad68035a6de2d201de81c6e2b18
jennyjean <jennyjean8675309@yahoo.com>
2019-04-04 11:56:56 -0400

    reverseint method w/out looking at solutions

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//Need to consider edge cases - negative numbers and numbers that end in zero(s), single digit numbers
+
+function reverseInt(n) {
+  //turn the number into an array
+  let nArray = n.toString().split('')
+  let newArray = []
+  //check to see if the number is negative
+  if (nArray[0] === '-') {
+    newArray.push(nArray[0])
+    nArray.shift()
+  }
+  let reversed = nArray.reverse()
+  //check to see if the number ends in zero(s)
+  while (reversed[0] === '0' && reversed.length > 1){
+    reversed.shift()
+  }
+  debugger;
+  let array = [...newArray, ...reversed]
+  return parseInt(array.join(''), 10)
+}
+
+reverseInt(90);
 
 module.exports = reverseInt;
