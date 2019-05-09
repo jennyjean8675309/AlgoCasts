9d31265b18f897e35c4f7784263c3ad9ecdf6dbe
jennyjean <jennyjean8675309@yahoo.com>
2019-05-09 14:37:30 -0400

    Work out the first solution for steps on my own

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Second code-along solution using rows and columns
+
+//      c0 |    c1 |    c2
+//    #    |  _    |   _
+//   r0    | r0    | r0
+// _______________________
+//      c0 |    c1 |    c2
+//    #    |  #    |   _
+//   r1    | r1    | r1
+// _______________________
+//      c0 |    c1 |    c2
+//    #    |  #    |   #
+//   r2    | r2    | r2
+
+//
+
+function steps(n) {
+  let steps = []
+}
+
+steps(3);
+
+// First solution I came up with on my own
+// function steps(n) {
+//   let step = '#'
+//
+//   if (step.length < n) {
+//     while (step.length < n) {
+//       step+= ' '
+//     }
+//   }
+//
+//   for (char in step) {
+//     console.log(step)
+//     step = step.replace(' ', '#')
+//   }
+// }
 
 module.exports = steps;
+
+// First iteration:  # _ _
+// Second iteration: # # _
+// Third iteration:  # # #
+
+// With each iteration I need to add a pound sign and subtract a space from the length of the string (n)
